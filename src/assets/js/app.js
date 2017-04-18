fffc471d3fc1af6e27203f91238a827c2157c121
Diallo Hudgins <d@diallohudgins.com>
2017-04-18 14:35:47 -0400

    added header styles, and testimonials block

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,28 @@
-$(document).foundation();
+(function($){
+	$(document).foundation();
+
+	// Orbit testimonial
+    var testimonial = new Foundation.Orbit($('#slideTestimonials'), {
+    	containerClass: 'slide-container',
+    	slideClass: 'item',
+    	prevClass: 'item-previous',
+    	nextClass: 'item-next',
+    	bullets: true
+    });
+
+
+    // Header
+    function stickyHeader() {
+	    $(window).scroll(function() {
+	    	var $header = $('.c-header-container ');
+	        if ($(this).scrollTop() >= 10) {
+	            $header.addClass('sticky');
+	        }
+	        else {
+	            $header.removeClass('sticky');
+	        }
+	    });
+
+    }
+    stickyHeader();
+})(jQuery);
