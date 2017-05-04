3d04784166e21237c2e33c5869f80ba9309d7e58
Diallo Hudgins <d@diallohudgins.com>
2017-05-04 17:32:17 -0400

    new slider and various fixes

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,14 +2,14 @@
 	$(document).foundation();
 
 	// Orbit testimonial
-    var testimonial = new Foundation.Orbit($('#slideTestimonials'), {
-    	containerClass: 'slide-container',
-    	slideClass: 'item',
-    	prevClass: 'item-previous',
-    	nextClass: 'item-next',
-    	bullets: true,
-    	boxOfBullets: 'o-bullets'
-    });
+    // var testimonial = new Foundation.Orbit($('#slideTestimonials'), {
+    // 	containerClass: 'slide-container',
+    // 	slideClass: 'item',
+    // 	prevClass: 'item-previous',
+    // 	nextClass: 'item-next',
+    // 	bullets: true,
+    // 	boxOfBullets: 'o-bullets'
+    // });
 
     // hamburger
     function hamburgerNav() {
